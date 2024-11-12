b3ee3be4673b3cc62dc54f0ce7be847817836338
rsanlazaro <rsanlazaro@hotmail.com>
2024-11-11 19:45:54 -0600

    first commit

---
+++ build/js/carousel.js
@@ -0,0 +1,10 @@
+$(function () {
+  $(".carousel-brands").slick({
+    centerMode: true,
+    // centerPadding: "150px",
+    // slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    accessibility: false
+  });
+});
